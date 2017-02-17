912155a129d01d12d3312d07a04c9f4dbc5b6721
Mikron <mikronia@gmail.com>
2017-02-17 20:06:54 +0100

    Tests for getters

--- tests/Domain/DocumentTest.php
+++ tests/Domain/DocumentTest.php
@@ -17,20 +17,47 @@ final class DocumentTest extends PHPUnit_Framework_TestCase
         $this->assertInstanceOf('Mikron\json2doc\Domain\Entity\Document', $character);
     }
 
+    /**
+     * @test
+     * @dataProvider correctDataProvider
+     * @param array $data
+     */
+    public function isJsonReturnedCorrectly(array $data)
+    {
+        $json = json_encode($data);
+
+        $character = new Document($json);
+
+        $this->assertEquals($json, $character->getJson());
+    }
+
+    /**
+     * @test
+     * @dataProvider correctDataProvider
+     * @param array $data
+     */
+    public function isArrayReturnedCorrectly(array $data)
+    {
+        $json = json_encode($data);
+
+        $character = new Document($json);
+
+        $this->assertEquals($data, $character->getArray());
+    }
 
     public function correctDataProvider()
     {
         return [
             [
-                json_encode([
+                [
                     "data" => "data",
-                ])
+                ]
             ],
             [
-                json_encode([
+                [
                     "name" => "name",
                     "data" => "data",
-                ])
+                ]
             ],
         ];
     }
